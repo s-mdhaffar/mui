1e1aae8d2431c3cb58275e0dd0d81dc73517ed9b
s-mdhaffar <s.mdhaffar@gmail.com>
2022-04-22 16:03:08 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 import { Stack } from '@mui/material';
 import { Box } from '@mui/system';
+import Navbar from './components/Navbar';
 
 function App () {
 	return (
 		<Box>
+			<Navbar />
 			<Stack direction="row" spacing={2} justifyContent="space-between">
 				<Sidebar />
 				<Feed />
